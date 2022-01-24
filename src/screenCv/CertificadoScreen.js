ce5a9be076ce3f1726e61f336dbf399d1a08eb0a
SantiagoJSdev <santiagoreyesmarin1986@gmail.com>
2022-01-24 15:46:03 -0500

    agregando certificados

--- src/screenCv/CertificadoScreen.js
+++ src/screenCv/CertificadoScreen.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import './certificado.css'
+import { useCounter } from '../hook/UseConter'
+import { dataCertificados } from './data-certificados/dataCertificados'
 
 export const CertificadoScreen = () => {
+
+const init = 0
+
+
+const {counter, increment, decrement, reset, setCounter} = useCounter(init)
+if ( (counter < 0 ) ) {
+return reset()
+} 
+if ( (counter > 3 ) ) {
+    return setCounter(3)
+} 
+
     return (
         <>
                 <div className='div-certificados'>
@@ -10,85 +24,36 @@ export const CertificadoScreen = () => {
                                 <span>Certificados</span>
                             </div>
 
-                            {/* <div className='certificado-circulo traslatecirculo'>
-                                <div className='certificado-cuadro'></div>
-                            </div>
-                            <div className='certificado-circulo traslatecirculo1'>
-                                <div className='certificado-cuadro'></div>
-                            </div>
-                            <div className='certificado-circulo traslatecirculo2'>
-                                <div className='certificado-cuadro'></div>
-                            </div>
-                            <div className='certificado-circulo traslatecirculo3'>
-                                <div className='certificado-cuadro'></div>
-                            </div>
-                            <div className='certificado-circulo traslatecirculo4'>
-                                <div className='certificado-cuadro'></div>
-                            </div>
-                            <div className='certificado-circulo traslatecirculo5'>
-                                <div className='certificado-cuadro'></div>
-                            </div>
-                            <div className='certificado-circulo traslatecirculo6'>
-                                <div className='certificado-cuadro'></div>
-                            </div> */}
-
 
                              <div className='certificado-interno'>
-                             {/*   <div className='certificadoCaja1'>
-                    
-                                    HENRY. Full Stack Web Developer - Bootcamp, intensivo con 700 horas de
-                                            práctica en tecnologías Web.
-                                    
-                                </div>
-                       
-                                <div className='certificadoCaja2'>
-                              
-                                    <a href='ude.my/UC-5631745b-1323-4f09-8e4a-5eb3cdb4f5c3' target='blank'>
-                                   
-                                          React Native: Aplicaciones nativas para IOS y Android </a>
-                                   
-                                </div>
-                                <div className='certificadoCaja3'>
-                                 
+
+                            {/* codigo nuevo///////////// */}
+
+                            <section className='slider'>
+                         
+                                    <h2>{ dataCertificados[counter].name }</h2>
+                                    <a href={dataCertificados[counter].url} target='_blank' >
+                                     <img 
+                                     src={dataCertificados[counter].img}
+                                     alt='img-curso'
+                                     />
+                                     </a>
+                                     <div className='slider-icons'>
                                   
-                                        <a href='ude.my/UC-5631745b-1323-4f09-8e4a-5eb3cdb4f5c3' target='blank'> 
-                                   
-                                        JavaScript Moderno</a>
-                                 
-                                </div>
-                                <div className='certificadoCaja4'>
-                     
-                                   <a href='https://www.udemy.com/certificate/UC-dd683800-8430-4afb-b393-227b39484c46/' target='blank'>
-                                  
-                                         Master en CSS3</a>
-                                    
-                                </div>
-                                <div className='certificadoCaja5'>
-                        
-                                  
-                                        <a href='https://www.udemy.com/certificate/UC-5631745b-1323-4f09-8e4a-5eb3cdb4f5c3/' target='blank'> 
+                                     <i 
+                                     onClick={decrement}
+                                     className="fas fa-chevron-left"></i>
+                                     <i
+                                     onClick={increment} className="fas fa-chevron-right"></i>
                                      
-                                        React: (Hooks y Mern)</a>
-                                    </div>
-                              
-                                <div className='certificadoCaja6'>
-                     
-                                  
-                                        <a href='https://www.udemy.com/certificate/UC-bb9529f9-cfd1-4d71-a009-f78ebd6b3c10/' target='blank'>
-                                
-                                             JavaScript Moderno </a>
-                                    
-                                </div>
-                                <div className='certificadoCaja7'>
-                      
-                                    
-                                        <a href='https://www.udemy.com/certificate/UC-57ab04e4-c5c3-494e-931c-1f71b90f40a7/' target='blank'>
+                                     </div>
+                             
                                
-                                             JavaScript ES9 </a>
-                                    
-                                </div>
-                            
-                            </div> */}
+                            </section>
+
+
+                             {/* codigo nuevo///////////// */}
+
                             </div>
                         </div>
             
---
+++ src/screenCv/data-certificados/dataCertificados.js
@@ -0,0 +1,30 @@
+
+
+
+export const dataCertificados = [
+    {
+        img: 'https://user-images.githubusercontent.com/87577172/150785659-54bf99a6-36de-4fb1-ae79-6cd8cf5adb7d.png',
+        name: 'Javascript-Es9',
+        url: 'https://ude.my/UC-57ab04e4-c5c3-494e-931c-1f71b90f40a7',
+        id: 1
+
+    },
+    {
+        img: 'https://user-images.githubusercontent.com/87577172/150786760-feb31c74-6dd7-43e6-8606-b53ddeac76a0.png',
+        name: 'Javascript-Moderno',
+        url: 'https://ude.my/UC-bb9529f9-cfd1-4d71-a009-f78ebd6b3c10',
+        id: 2
+    },
+    {
+        img: 'https://user-images.githubusercontent.com/87577172/150787384-81999e16-1828-4285-ba41-890eb94c608c.png',
+        name: 'React: Hooks y Mern',
+        url: 'https://ude.my/UC-5631745b-1323-4f09-8e4a-5eb3cdb4f5c3',
+        id: 3
+    },
+    {
+        img: 'https://user-images.githubusercontent.com/87577172/150788318-a06d883a-e9d5-485c-a5ce-c3ef54c9b9e7.png',
+        name: 'Master en CSS',
+        url: 'https://ude.my/UC-dd683800-8430-4afb-b393-227b39484c46',
+        id: 4
+    },
+]
